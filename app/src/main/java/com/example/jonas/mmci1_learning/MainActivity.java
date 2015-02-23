522f5805881b655caa4af53f32370b9f41619854
Jonas <jonaskraus@gmx.de>
2015-02-23 17:18:48 +0100

    correct reset of the known list counter

--- app/src/main/java/com/example/jonas/mmci1_learning/MainActivity.java
+++ app/src/main/java/com/example/jonas/mmci1_learning/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends ActionBarActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     knownList = dbManager.getIdsWithRatingASC();
-                    countKnownList = 0;
+                    countKnownList = 0; // resets the counter
                     /*
                     int i = 0;
                     Log.d("knoenList", knownList.get(0)+" "+ knownList.size());
